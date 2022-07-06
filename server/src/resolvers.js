689015235e8957ff7b15fd111b53e3b533109970
sunaycansev <fsunaycansev@gmail.com>
2022-07-06 13:51:26 +0300

    Update resolvers.js

--- server/src/resolvers.js
+++ server/src/resolvers.js
@@ -2,7 +2,14 @@ const resolvers = {
   Query: {
     // returns an array of Tracks that will be used o populate
     // the homepage grid of our client
-    tracksForHome: () => {},
+    tracksForHome: (_, __, { dataSources }) => {
+      return dataSources.trackAPI.getTracksForHome();
+    },
+  },
+  Track: {
+    author: ({ authorId }, _, { dataSources }) => {
+      dataSources.trackAPI.getAuthour(authorId);
+    },
   },
 };
 
