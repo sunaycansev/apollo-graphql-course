6123c3cac56f340c35e1f3f70abe548a1556766e
sunaycansev <fsunaycansev@gmail.com>
2022-07-06 12:13:49 +0300

    Create resolvers.js

---
+++ server/src/resolvers.js
@@ -0,0 +1,9 @@
+const resolvers = {
+  Query: {
+    // returns an array of Tracks that will be used o populate
+    // the homepage grid of our client
+    tracksForHome: () => {},
+  },
+};
+
+module.exports = resolvers;
