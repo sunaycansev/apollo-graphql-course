bf3b1dd401e83b98d260961acf280c16dd178662
sunaycansev <fsunaycansev@gmail.com>
2022-07-07 16:16:11 +0300

    add mutation to client

--- client/src/containers/track-card.js
+++ client/src/containers/track-card.js
@@ -3,15 +3,36 @@ import styled from "@emotion/styled";
 import { colors, mq } from "../styles";
 import { humanReadableTimeFromSeconds } from "../utils/helpers";
 import { Link } from "@reach/router";
+import { gql, useMutation } from "@apollo/client";
 /**
  * Track Card component renders basic info in a card format
  * for each track populating the tracks grid homepage.
  */
+// mutation to increment a track's number of views
+// exported for testing
+export const INCREMENT_TRACK_VIEWS = gql`
+  mutation IncrementTrackViews($incrementTrackViewsId: ID!) {
+    incrementTrackViews(id: $incrementTrackViewsId) {
+      code
+      success
+      message
+      track {
+        id
+        numberOfViews
+      }
+    }
+  }
+`;
 const TrackCard = ({ track }) => {
   const { title, thumbnail, author, length, modulesCount, id } = track;
-
+  const [incrementTrackViews] = useMutation(INCREMENT_TRACK_VIEWS, {
+    variables: { incrementTrackViewsId: id },
+    onCompleted: (data) => {
+      console.log(data);
+    },
+  });
   return (
-    <CardContainer to={`/track/${id}`}>
+    <CardContainer to={`/track/${id}`} onClick={incrementTrackViews}>
       <CardContent>
         <CardImageContainer>
           <CardImage src={thumbnail} alt={title} />
